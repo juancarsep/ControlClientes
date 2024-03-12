76e7277e0fe9a93146ae88eee4175fe9b655be60
Juan <carballosepejuan@gmail.com>
2024-03-11 23:37:17 -0300

    dependencias, clase cliente, conexion a bd, servlet controlador y el index hechos

---
+++ src/main/java/dominio/Cliente.java
@@ -0,0 +1,91 @@
+package dominio;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private double saldo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id) {
+        this.id = id;
+    }
+
+    public Cliente(String nombre, String apellido, String email, String telefono, double saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    
+    
+    public Cliente(int id, String nombre, String apellido, String email, String telefono, double saldo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + ", saldo=" + saldo + '}';
+    }
+    
+    
+}
