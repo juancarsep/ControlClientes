42afae70305130933a0bec93e44f4605a2afd74a
Juan <carballosepejuan@gmail.com>
2024-03-12 22:49:30 -0300

    logica de el agregado de nuevo cliente completada

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -1,4 +1,3 @@
-
 package web;
 
 import datos.ClienteDaoJDBC;
@@ -10,31 +9,71 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/ServletControlador")
-public class ServletControlador extends HttpServlet{
+public class ServletControlador extends HttpServlet {
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.accionDefault(request, response);
+    }
+    
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Cliente> clientes = new ClienteDaoJDBC().listar();
         System.out.println(clientes);
-        request.setAttribute("clientes", clientes);
-        request.setAttribute("totalClientes", clientes.size());
-        request.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
         
-      
-        request.getRequestDispatcher("clientes.jsp").forward(request, response);
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("clientes", clientes);
+        sesion.setAttribute("totalClientes", clientes.size());
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
+        
+        //request.getRequestDispatcher("clientes.jsp").forward(request, response); ESTE CODIGO PROVOCA QUE EL URL NO CAMBIE Y SI ACTUALIZAMOS SE VUELVE A AGREGAR EL NUEVO REGISTRO
+        response.sendRedirect("clientes.jsp");
         
         
     }
     
-    private double calcularSaldoTotal(List<Cliente> clientes){
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+        if (accion != null) {
+            switch (accion) {
+                case "insertar":
+                    this.insertarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+                
+            }
+        }else{
+            this.accionDefault(request, response);
+        }
+    }
+    
+    private double calcularSaldoTotal(List<Cliente> clientes) {
         double saldoTotal = 0;
         
-        for(Cliente cliente : clientes){
+        for (Cliente cliente : clientes) {
             saldoTotal += cliente.getSaldo();
-        }       
+        }
         return saldoTotal;
+    }
+    
+    private void insertarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Recuperamos los valores del formulario agregar cliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");        
+        //TODO si da error al mandar un saldo nulo o vacio, agregar un if preguntando si el saldo es diferente de null antes de hacer la conversion
+        double saldo = Double.parseDouble(request.getParameter("saldo"));
+        //Creamos al nuevo cliente
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
+        //Insertamos el cliente en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
+        System.out.println("registros modificados: " + registrosModificados);
+        this.accionDefault(request, response);
     }
     
 }
